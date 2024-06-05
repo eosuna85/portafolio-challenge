7dc91a030eeb6ea67d73893295a37e668eacd262
eosuna85 <erika.osuna85@gmail.com>
2024-06-05 14:55:14 -0300

    17, pdf

--- js/curriculo.js
+++ js/curriculo.js
@@ -1,6 +1,6 @@
 const verCurriculo = document.getElementById('curriculo');
 
 verCurriculo.addEventListener('click',()=>{
-    let rutaPDF = "../pdf/Profile.pdf";
+    let rutaPDF = "../pdf/profile.pdf";
     window.open(rutaPDF, "_blank");
 });
