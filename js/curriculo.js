117960562aac45dbfdf60a582ffe6eed1d83206c
eosuna85 <erika.osuna85@gmail.com>
2024-06-05 15:14:27 -0300

    19

--- js/curriculo.js
+++ js/curriculo.js
@@ -1,6 +1,8 @@
-const verCurriculo = document.getElementById('curriculo');
+document.addEventListener('DOMContentLoaded', () => {
+    const verCurriculo = document.getElementById('curriculo');
 
-verCurriculo.addEventListener('click',()=>{
-    let rutaPDF = "../pdf/CV_Profile.pdf";
-    window.open(rutaPDF, "_blank");
+    verCurriculo.addEventListener('click', () => {
+        let rutaPDF = "pdf/CV_Profile.pdf";
+        window.open(rutaPDF, "_blank");
+    });
 });
